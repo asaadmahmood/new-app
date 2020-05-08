d1103bed514af85ec28342c6343ed93d022e81a3
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2020-05-09 02:02:23 +0500

    Updating code

--- src/actions/members.js
+++ src/actions/members.js
@@ -1,82 +1,82 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
 
-export const addMember =(member) => ({
-    type: 'ADD_MEMBER',
-    member
+export const addMember = (member) => ({
+  type: 'ADD_MEMBER',
+  member
 });
 export const startAddMember = (memberData = {}) => {
-    return (dispatch, getState) => {
-      const uid = getState().auth.uid;
-      const {
-        name= '',
-        age = '',
-        sex = '',
-        packege = 0,
-        address = '',
-        createdAt = 0
-      } = memberData;
-      const member = { name, age, sex, packege, address , createdAt };
-  
-      database.ref(`users/${uid}/members`).push(member).then((ref) => {
-        dispatch(addMember({
-          id: ref.key,
-          ...member
-        }));
-      });
-    };
-  };
-// REMOVE_MEMBER
-export const removeMember = ({ id } = {}) => ({
-    type: 'REMOVE_MEMBER',
-    id
-  });
-  export const startRemoveMember = ({id} = {}) => {
   return (dispatch, getState) => {
     const uid = getState().auth.uid;
-   return database.ref(`users/${uid}/members/${id}`).remove().then(() =>{
-      dispatch(removeMember({id}));
+    const {
+      name = '',
+      age = '',
+      sex = '',
+      packege = 0,
+      address = '',
+      createdAt = 0
+    } = memberData;
+    const member = { name, age, sex, packege, address, createdAt };
+
+    database.ref(`users/${uid}/members`).push(member).then((ref) => {
+      dispatch(addMember({
+        id: ref.key,
+        ...member
+      }));
     });
   };
+};
+// REMOVE_MEMBER
+export const removeMember = ({ id } = {}) => ({
+  type: 'REMOVE_MEMBER',
+  id
+});
+export const startRemoveMember = ({ id } = {}) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/members/${id}`).remove().then(() => {
+      dispatch(removeMember({ id }));
+    });
   };
-  // EDIT_MEMBER
+};
+// EDIT_MEMBER
 export const editMember = (id, updates) => ({
   type: 'EDIT_MEMBER',
   id,
   updates
 });
-export const startEditMember = (id, updates) =>{
+export const startEditMember = (id, updates) => {
   return (dispatch, getState) => {
     const uid = getState().auth.uid;
-   return database.ref(`users/${uid}/members/${id}`).update(updates).then(() =>{
-      dispatch (editMember(id, updates));
+    return database.ref(`users/${uid}/members/${id}`).update(updates).then(() => {
+      dispatch(editMember(id, updates));
     });
   };
 };
 
-  //SET_MEMBERS
+//SET_MEMBERS
 
 const setMembers = (members) => ({
-    type: 'SET_MEMBERS',
-    members
-  });
-  
-  export const startSetMembers = () => {
-    return (dispatch, getState) => {
-      const uid = getState().auth.uid;
-      return database.ref(`users/${uid}/members`)
-        .once('value')
-        .then((snapshot) => {
-          const members = [];
-  
-          snapshot.forEach((childSnapshot) => {
-            members.push({
-              id: childSnapshot.key,
-              ...childSnapshot.val()
-            });
+  type: 'SET_MEMBERS',
+  members
+});
+
+export const startSetMembers = () => {
+  console.log('asdsad');
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/members`)
+      .once('value')
+      .then((snapshot) => {
+        const members = [];
+
+        snapshot.forEach((childSnapshot) => {
+          members.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
           });
-          dispatch(setMembers(members));
         });
-    };
+        dispatch(setMembers(members));
+      });
   };
-  
+};
--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -1,35 +1,46 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import MemberListItem from './MemberListItem';
 import selectMembers from '../selectors/members';
 
-export const MemberList =(props) => (
-    <div className="content-container">
-    <div className="list-header">
-      <div className="show-for-mobile">Members</div>
-      <div className="show-for-desktop">Members</div>
-     <div className="show-for-desktop">Packege</div>
-    </div>
-    <div className="list-body">
-    {
-      props.members.length === 0 ? (
-<div className="list-item list-item--message">       
-  <span>No member</span>
-</div>
-) : (
-    props.members.map((member) => {
-      return <MemberListItem key={member.id} {...member} />;
-    })
-  )
-}
-</div>
-</div>
-    );
+export class MemberList extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
 
-    const mapStateToProps = (state) => {
-        return {
-          members: selectMembers(state.members, state.filters)
-        };
-      };
-      
-      export default connect(mapStateToProps)(MemberList);
+  render() {
+    return (
+      <div className="content-container">
+        <div className="list-header">
+          <div className="show-for-mobile">Members</div>
+          <div className="show-for-desktop">Members</div>
+          <div className="show-for-desktop">Packege</div>
+        </div>
+        <div className="list-body">
+          {this.props.members}
+          {
+            this.props.members.length === 0 ? (
+              <div className="list-item list-item--message">
+                <span>No member</span>
+              </div>
+            ) : (
+                props.members.map((member) => {
+                  return <MemberListItem key={member.id} {...member} />;
+                })
+              )
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    // members: selectMembers(state.members, state.filters)
+    members: state.members
+  };
+};
+
+export default connect(mapStateToProps)(MemberList);
